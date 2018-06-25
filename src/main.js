a03555068cddf36319839151c7a4dea54c684e45
Royee <luoyu@gdgyun.com>
2018-06-25 09:16:12 +0800

    [^] 规范文件引入 [+] 修改监听端口，和跨域代理

--- src/main.js
+++ src/main.js
@@ -4,27 +4,25 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-
 // Element UI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-
 // Cookie
 import VueCookie from 'vue-cookie'
-Vue.use(VueCookie)
-
 // Awesome Icon
 import Icon from 'vue-awesome/components/Icon'
 import 'vue-awesome/icons'
-Vue.component('icon', Icon)
-
-Vue.config.productionTip = false
-
 // Custom Css
 import './assets/styles/element-reset.scss'
 import './assets/styles/animation.scss'
 import './assets/styles/transition.scss'
+
+Vue.use(ElementUI)
+Vue.use(VueCookie)
+
+Vue.component('icon', Icon)
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
