0ea4b2d460557707ffce026520a7fd6b0c76e255
chenleibing <biner@91this.com>
2018-07-02 18:08:23 +0800

    [+]分页组件、消息组件

---
+++ src/components/community/common/MessageBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <el-button type="text" ></el-button>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    messages: Object
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.$on('messageBox', () => {
+        this.messageBox()
+      })
+    })
+  },
+  methods: {
+    messageBox () {
+      this.$confirm(this.messages.msg, this.messages.title, {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$emit('childMessage', true)
+      }).catch(() => {
+        this.$emit('childMessage', false)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
